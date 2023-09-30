e294a9389499ad47714d7735ef6b1d712c4749dd
RyUKEnza <kenza-raj@hotmail.fr>
2023-09-29 18:08:09 -0700

    Betty style fix

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,15 +3,15 @@
 /**
  * get_bit - Returns the value of a bit at a given index.
  * @n: The unsigned long int to extract the bit from.
- * @index: The index of the bit to get (0-based).
+ * @index: The bit index (0-based).
  *
- * Return: The value of the bit at the specified index, or -1 if an error occurs.
+ * Return: The bit value at the index, or -1 on error.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
-		return -1;
+		return (-1);
 
-	return (n >> index) & 1;
+	return ((n >> index) & 1);
 }
 
