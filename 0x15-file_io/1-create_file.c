70eede0373ce0bfa0241e9f3688719b6c03de33f
RyUKEnza <kenza_raj@hotmail.fr>
2024-03-31 16:49:17 -0700

    betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,30 +13,30 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, write_count;
-    ssize_t text_length = 0;
+	int fd, write_count;
+	ssize_t text_length = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[text_length] != '\0')
-            text_length++;
+	if (text_content != NULL)
+	{
+		while (text_content[text_length] != '\0')
+			text_length++;
 
-        write_count = write(fd, text_content, text_length);
-        if (write_count != text_length)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+		write_count = write(fd, text_content, text_length);
+		if (write_count != text_length)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
-    close(fd);
-    return (1);
+	close(fd);
+	return (1);
 }
 
